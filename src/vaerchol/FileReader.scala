83e02d5cadd97591f6b83f41aeadcda86290df18
Prasanna Kumar <prassee@prassee.me>
2015-04-08 17:01:18 +0530

    initial commit

--- src/vaerchol/FileReader.scala
+++ src/vaerchol/FileReader.scala
@@ -2,8 +2,10 @@ package vaerchol
 
 import scala.io.Source
 
+/*
+ * தமிழ் நிரல் 
+ */
 object FileReader extends App {
-  val file = Source.fromFile("/Users/prasannakumar/tamil.txt", "UTF-8")
+  val file = Source.fromFile("~/tamil.txt", "UTF-8")
   println(file.getLines().mkString(" ").split(" ").mkString(" - "))
 }
-
